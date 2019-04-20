080cd02f1ae7f10b1ff019da6ba483ba6503ced2
huangdeyi <1534527968@qq.com>
2019-04-20 15:39:34 +0800

    咋回事(20190420 15:40)

--- exam/src/git/Test.java
+++ exam/src/git/Test.java
@@ -2,6 +2,6 @@ package git;
 
 
 public class Test {
-    private static String update = "what?????";
+    private static String update = "what";
 	private static final String git = "git»ù±¾Ê¹ÓÃ"; 
 }
